911ac6f5dc493bc9de066946d9618eb6e49eebc8
weam <weamibrahim46@gmail.com>
2023-04-29 23:13:26 +0300

    add function getallrecord and make show page

--- models/DbHandler.php
+++ models/DbHandler.php
@@ -1,5 +1,5 @@
 <?php
 interface DbHandler{
     public function connect();
-  
+    public function get_all_record_paginated($field=array(),$start=0);
 }
--- models/MySQLHandler.php
+++ models/MySQLHandler.php
@@ -28,6 +28,54 @@
          
     }
  
+    public function get_all_record_paginated($field=array(),$start=0)
+    {
+         $table=$this->_table;
+        if(empty($field))
+        {
+                $sql="select * from `$table`";
 
+        }
+        else
+        {
+             $sql="select *";
+             foreach($field as $f){
+                                 $sql=" `$f` ,";
+                                  }
+              $sql.="from `$table`";
+              $sql=str_replace(", from","from",$sql);
+        }
+             //$sql.="limit $start ," . __RECORDS_PER_PAGE__ ;
+
+        return $this->getResult($sql);
+
+    }
+    private function getResult($sql)
+    {
+      if(__Debug_Mode__=== 1)
+      {
+           echo "<h3>sent quary:</h3>".$sql."<br>";
+      } 
+            $_handler_result=mysqli_query($this->_db_handler,$sql);
+            $result=[];
+
+           if(!$_handler_result)
+           {
+                return false;
+
+                
+            }
+            while($row=mysqli_fetch_array($_handler_result,MYSQLI_ASSOC))
+            {
+
+             $result[]=array_change_key_case($row);
+             }
+            return $result;
+         
+    }
+    public function disconnect() {
+     if ($this->_db_handler)
+         mysqli_close($this->_db_handler);
+ }
 
    }
