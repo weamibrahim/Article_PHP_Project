ceaeea1b2bd68172117b79406e193fc9a7ed4b42
violageorge <violageorgekamalmansour.com>
2023-04-30 18:14:29 +0300

    edit index

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
             require_once("vendor/autoload.php");
-            include "./adminHeader.php";
-            include "./sidebar.php";
+           
                    ?>
             <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -10,24 +9,24 @@
         <!DOCTYPE html>
 <html>
 <head>
-  <title>Admin</title>
+  <title>Article</title>
 </head>
 <body >
      <?php
 if(isset($_GET["group"])&&!isset($_GET["delete"])){
     if($_GET["group"]==intval($_GET["group"])){
     
-             require_once("views/Groups/Groupedit.php");
+             require_once("views/Groups/update.php");
          }
          elseif(($_GET["group"])=='delete'){
-             require_once("views/Groups/Groupdelete.php"); 
+             require_once("views/Groups/delete.php"); 
          }
          else{
-             require_once("views/Groups/GroupsView.php"); 
+             require_once("views/Groups/show.php"); 
          }
      }
 
-     elseif(isset($_GET["user"])&&!isset($_GET["delete"])){
+    /* elseif(isset($_GET["user"])&&!isset($_GET["delete"])){
         if($_GET["user"]==intval($_GET["user"])) {
             require_once("views/Users/UsersEdit.php");
         }
@@ -40,7 +39,7 @@ if(isset($_GET["group"])&&!isset($_GET["delete"])){
         else{
             require_once("views/Users/UsersView.php"); 
         }
-    }
+    }*/
 
     elseif(isset($_GET["article"])&&!isset($_GET["delete"])){
         if($_GET["article"]==intval($_GET["article"])){
