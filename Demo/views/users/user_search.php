72509879dcacf8d2e12555b68eee7fdf1a821a90
weam <weamibrahim46@gmail.com>
2023-05-02 14:14:19 +0300

    update search user

--- Demo/views/users/user_search.php
+++ Demo/views/users/user_search.php
@@ -14,64 +14,96 @@ require_once("../../vendor/autoload.php");
   <!-- Content Header (Page header) -->
   <div class="content-header">
 
-<div class="container">
+  <div class="container">
     <form class=" mt-5 " method="post" class="container" enctype="multipart/form-data">
     <div class="shadow p-3 mb-5 mt-4  rounded-4 container ">
         <p class="text-center fs-1 fst-italic" style="color: #BC8CE9; text-shadow: 1px 2px #A8BBC9; margin-top: -50px">Search user</p>
         <div class="form-group">
-            <input type="text" class="form-control" id="name" name="name" placeholder="Search by name or group">
+                <input type="text" class="form-control" id="name" name="name" placeholder="Search by name or group">
+            </div>
+
         </div>
-    </div>
-    <div class="d-flex justify-content-center">
-        <button type="submit" class="btn btn-info" name="search_by_name_desc" style="background-color: #B988E9; border-color: #B988E9; color:white">Search by name or group</button>
-    </div>
+        <div class="d-flex justify-content-center">
+            <button type="submit" class="btn btn-info" name="search_by_name_group" style="background-color: #B988E9; border-color: #B988E9; color:white">Search by name or group</button>
+        </div>
+
     </form>
     <?php
-    $db = new MySQLHandler("groups");
+    $db = new MySQLHandler("users");
     //$id=$_GET['updateId'];
-
+    $dbs = new MySQLHandler("groups");
+    $groups = $dbs->get_all_record_paginated(array(), 0);
+    
+    $group_names = array();
+    
+    foreach($groups as $group) {
+      $group_names[$group["id"]] = $group["name"];
+    }
     //var_dump($groups);
-    if (isset($_POST['search_by_name_desc'])) {
+    if (isset($_POST['search_by_name_group'])) {
         $name = $_POST['name'];
-    $results = $db->search('name', $name, 'description', $name);
 
+        $results = $db->search('name', $name, 'groupid', $name);
+       
 
     ?>
-        <table class="table  mt-5 ">
+        <table class="table table-striped  mt-5 ">
 
             <thead class="table-dark text-center">
                 <tr>
-                    <th scope="col" style="background-color:#D3B2B7">#</th>
-                    <th scope="col" style="background-color:#D3B2B7">Icon</th>
-                    <th scope="col" style="background-color:#D3B2B7">Name</th>
-                    <th scope="col" style="background-color:#D3B2B7">Decription</th>
+                    <th scope="col">Id</th>
+                    <th scope="col">userName</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">name</th>
+              
+                    <th scope="col">group name</th>
+                    <th scope="col">type</th>
                 </tr>
             </thead>
 
-            <tbody class="text-center" style="color: white;">
+            <tbody class="text-center">
 
 
                 <?php
 
                 if (!empty($results)) {
                     foreach ($results as $result) {
+                        //echo $result['name'] . '<br>';
+
 
                 ?>
+
+
+
+
+
+
 
                         <tr>
 
                             <td><?php echo $result['id'] ?></td>
                             <!-- $result['icon'] -->
-                            <td><?php echo '<img  style ="wigth:20px;height:20px;"src="../../uploads/' . $result["icon"] . '">'; ?></td>
+
+                            <td><?php echo $result['username'] ?></td>
+                            <td><?php echo $result['email'] ?></td>
                             <td><?php echo $result['name'] ?></td>
-                            <td><?php echo $result['description'] ?></td>
+                           
+                            <td><?php echo $group_names[$result['groupid']] ?></td>
+                            <td><?php echo $result['type'] ?></td>
+
+
                         </tr>
+
+
+
 
                     <?php
                     }
                 } else {
 
                     ?>
+                    <td> no record </td>
+                    <td> no record </td>
                     <td> no record </td>
                     <td> no record </td>
                     <td> no record </td>
